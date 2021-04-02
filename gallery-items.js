8002a2171a38700751725b0df440fc287cfef619
Oksana Palamarchuk <8444186@gmail.com>
2021-04-02 08:51:33 +0200

    Lazy loading added

--- gallery-items.js
+++ gallery-items.js
@@ -62,4 +62,109 @@ export default [
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
+  {
+    preview:
+      'https://pixabay.com/get/g5242606bcd2e690f9980fa7db2477e042c78c8544741d0837aad373fd2239ce5223ec3c2ed3d54c87897459a4d62adbe6b6c20bdeb2fd1d9e1707a2d2b50315f_340.jpg',
+    original:
+      'https://pixabay.com/get/gf7bb54a282125387235c25bdbfad719b1db3c75cf774598c76a1fb63f6e777f09696ae48fc76e65aff2777ecbc8690c68e0dfea76771a8a5f46f763495feabb0_1280.jpg',
+    description: 'Two Yellow Roses',
+  },
+ {
+    preview:
+      'https://pixabay.com/get/g1e25c05189d74fea80438500371c7e7a7d3e3ab99b4d3afe7eefb0dbf5fdc636a7168c8f98015c95b6133fffaf40d1a841b5514e5b0fb5a7234b71df4cfabb59_340.jpg',
+    original:
+      'https://pixabay.com/get/gd79f1995d079444948736e8f2515612107bfa2cc989232fb005f0cdb34100d229cb4255824476de3ba5634de38f6f71c0702771ebf3a4a2aca5b165bdf7f5f34_1280.jpg',
+    description: 'Sunflower',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/g7295d5302b2c97c952370a43cd61dc576a3eeb904f90d45e897b95667970065e62a130e0fa91fdd18d2f589e0226ef814b9c081b5e67066dfb9db243a1dbd2a4_340.jpg',
+    original:
+      'https://pixabay.com/get/g3cd33b66392fff4ba62ce6bce6b7f3199821e67619073fb1e30dc79f921e6e5d0d388aa339769f2ab7a28929c195a94ecd37ce1d9227484837a5f92fadec8658_1280.jpg',
+    description: 'Spring Bird',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/gb1ce7e8442f9b94c5fb93a949d8b14f6bba56bbc7702474d07c1a4aa4be16a5c46a39a13b65727cf71aaf9aea7d283807d92cf6e0f98580e87c65dce870068ea_340.jpg',
+    original:
+      'https://pixabay.com/get/g3ec05375a5ac5a1b1bf8e8bbebf5843701a59c6b39dbea78516aa30b2b1509d7b5c9a1fcdf71bf4c753a4e0bf6449b2bb32d4883fa48b99427c4c0c50ab325c2_1280.jpg',
+    description: 'Sunflower in the Vase',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/gba88752bd8ea522eb9d0e077a65a9539837055bef796e3f7c7e2148f4b28f054f86adc20cd2d7a23a66b5e7ceec192d5_340.jpg',
+    original:
+      'https://pixabay.com/get/g6dc2512434faa9b064ea4f789c2476dbc6e786df1be6f9d771989c333e80b09f37af808ea4a43678de42e0a86d82d24cdf1137785c17abb173c7751e9c709291_1280.jpg',
+    description: 'Yellow Tulips',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/gdebe66dab1d87a2a405f0457f6d32c252d49f6e4f0d72346b5a7af5ec0a8de73da724e33f7424fbd2bc46363a6f0961f_340.jpg',
+    original:
+      'https://pixabay.com/get/gacd7dfddaae506a75888038ee01665225dd6d4cbbe12ff5bd1d08111afceb572180d9560b50dd2b75cc9c124348bf436ca924c354dd71b9d1556ac6618d2e66f_1280.jpg',
+    description: 'Flower in the Desert',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/g93ca67e4384862819d2768e9d259da75027dc01e7974d40e8d05f09cf092897c590953af97f6857df33a96ae62363e4a0acc399389485988b8ad8ce53aabeb25_340.jpg',
+    original:
+      'https://pixabay.com/get/gb0c179797203dc9abfa9d5b1c71ac84c270e5e40a4f29ae3eedcaea6ff1eccb04f05ee263d54c9c29cf71905824274f05d8a4335b33effe086468be7e684e88f_1280.jpg',
+    description: 'Dew on the Flower Petals',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/gf5001fb2b956cb4367aac97e893ada18cc28127e40f8bca1af1a19f386ce046b538da80b74a622704c296f2d28f0d94f_340.jpg',
+    original:
+      'https://pixabay.com/get/g8ac8c436780c993971875453457599e1a554d6d25e35c5c03bab5ffca42e906743ee3ee6245fb36fa4384ee5016fb27e049c45df140b57826f29f3c35d2d26a5_1280.jpg',
+    description: 'Flower Upside-Down',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/gda4201cca5ccd5b266d378447b3de5c5ec69f50144167070a7e7c52d7ce235c15ca4d9695699628085a609515db8e2470879ba9fc5a57b570652715b615a7024_340.jpg',
+    original:
+      'https://pixabay.com/get/g56891eaf24f4d9304ca772156a8eafa90488b3498fa3843ef27061a539e2b82e0bb336642a704a6bf7687589b72c8b5eebeb54e6308b4d1f07eb90ecc256eb20_1280.jpg',
+    description: 'Poppy in the field',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/g137f8325ecae7830ac95a49de0b678a53ffef2f0f54606278f870bf61be6326a5bbcb6726a60f9a328b1a89380d6ec26_340.jpg',
+    original:
+      'https://pixabay.com/get/g47df038c3f359a23ce1659619157249333863fc3bdb5c0b5eefa8bf247c1e66e9650b57eba89ea85b022411962efa9a302f4d04d5fed6a491d3eba2ee67764f6_1280.jpg',
+    description: 'Bicycle',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/g5809fc24819fccd795a35efd9c50795b4d91657df1ad4fa48323e2813106c50ec6ba4b0ed64129bba25b2111897581b8_340.jpg',
+    original:
+      'https://pixabay.com/get/gad0b7a7ab7f8e4d34f28f0c81d9d74272dec77d16941164aa518d42a78ae93499add47b5c5ef26a0455675099530c31a1cc41ce2ff64c5dde7dfd8222d048932_1280.jpg',
+    description: 'Dandelions',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/g0f773ce90bdc5e012b3b2907d138a59599f57d5906aef09ac785feaa447865d258f36369fcc25f160209784bd4356d74ec4bef7133e6edf6320385c07eee26af_340.jpg',
+    original:
+      'https://pixabay.com/get/g0ec06f0f51297e1ce94cfed50b03d6e0a025216c499687d5b4a250066ec046738608501340393e0e715a1cad02c04922ed934b7e16e93ca9212184d41301446c_1280.jpg',
+    description: 'Yellow Flowers',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/g7fb6f911a1e17b0788e88299cac72016cbfbc418ba14228f030b16d4ffb4809dfa3cff95a291f9f6e1a788f6cce958545d39945e35c2e85728f59b5d0828131e_340.jpg',
+    original:
+      'https://pixabay.com/get/g496a86fc3161ca416cce5baa07af1ffe6f6de53ae6c3fe2dd4b570a0ab62b4bee0e4f45eb9dde46519d7967a9f866ba0bec8642190a045bfe263096cfddf7dea_1280.jpg',
+    description: 'Mountains',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/ge37ed14852c0ecff7fef70000edbfc55af544d534dd7ac959dfe0b63ab7f6d7a69136d98ad3d6e6d6694c362550f9322e8699543ee4b7f387124268f09693485_340.jpg',
+    original:
+      'https://pixabay.com/get/g77d628e01b5b2c02e8eee2d5e99b92573e496339bd1a6c403520c8c5597cf035201294a242b66a3052c8a6d75f44a75464f4be5871e02e1bd14a7bc246fc4190_1280.jpg',
+    description: 'Yellow Flowers',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/g14868edfb795733d4cf70d356b84f7dca5610a300f6b522f7ceab7dd74a9f691de1878fc8a56e3c8ee6877d16b8f314c247675a93f794065f476ed02a664620c_340.jpg',
+    original:
+      'https://pixabay.com/get/g0e007ed2fc7816993a16f2d4cdd528c8e8c2c352d9e84025782c346dca5eb0f7b13a7f6094587ebf7587936cddc60157022a3b65714fca687a79a98f15463de3_1280.jpg',
+    description: 'Elephant',
+  },
 ];
--- gallery.js
+++ gallery.js
@@ -1,56 +1,13 @@
-/* 
-Создай галерею с возможностью клика по ее элементам и просмотра полноразмерного изображения 
-в модальном окне. Превью результата посмотри по ссылке.
-
-Разбей задание на несколько подзадач:
-
-+ Создание и рендер разметки по массиву данных и предоставленному шаблону.
-+ Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-+ Открытие модального окна по клику на элементе галереи.
-Подмена значения атрибута src элемента img.lightbox__image.
-Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
-Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, 
-чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
-
-В файле gallery-items.js есть массив объектов содержащих информацию о изображениях: 
-маленькое изображение, оригинальное и описание.
-
-Разметка элемента галереи
-Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе img, 
-и указываться в href ссылки (это необходимо для доступности).
-
-<li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
-    />
-  </a>
-</li>
-
-Модальное окно для полноразмерного изображения
-      Для того чтобы открыть, необходимо добавить на div.lightbox CSS-класс is-open
-*/
-
 import galleryItems from "./gallery-items.js";
 
-const galleryContainer = document.querySelector('.js-gallery');
-console.log(galleryContainer);
-const modalWindow = document.querySelector('.lightbox');
-console.log(modalWindow);
-
-
+const galleryContainerRef = document.querySelector('.js-gallery');
+const modalWindowRef = document.querySelector('.js-lightbox');
+const lightboxImageRef = modalWindowRef.querySelector('.lightbox__image');
 const galleryMarkup = createGalleryCardsMarkup(galleryItems);
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainerRef.insertAdjacentHTML('beforeend', galleryMarkup);
 
-galleryContainer.addEventListener('click', onGalleryContainerClick);
-
+galleryContainerRef.addEventListener('click', onGalleryContainerClick);
 
 function createGalleryCardsMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
@@ -61,8 +18,9 @@ function createGalleryCardsMarkup(galleryItems) {
             href="${original}"
         >
             <img
-            class="gallery__image"
-            src="${preview}"
+            loading="lazy"
+            class="gallery__image lazyload"
+            data-src="${preview}"
             data-source="${original}"
             alt="${description}"
             />
@@ -74,19 +32,64 @@ function createGalleryCardsMarkup(galleryItems) {
 }
 
 function onGalleryContainerClick(event) {
+  event.preventDefault();
 
     if (!event.target.classList.contains('gallery__image')) {
         return;
     }
-    return event.target.dataset.source;
-
+  const galleryImage = event.target;
+ 
+  return galleryImage.dataset.source;
 }
 
-const galleryElement = document.querySelector('.gallery__item');
-console.log(galleryElement);
-
-galleryElement.addEventListener('click', onGalleryElementClick);
+const galleryElementsRef = document.querySelectorAll('.gallery__item');
+galleryElementsRef.forEach(element => {
+  element.addEventListener('click', onGalleryElementClick);
+})
 
 function onGalleryElementClick() {
-    modalWindow.classList.add('is-open');
+  window.addEventListener('keydown', onEscKeyPress);
+  modalWindowRef.classList.add('is-open');
+  lightboxImageRef.src = onGalleryContainerClick(event);
+}
+
+const modalCloseBtnRef = document.querySelector('[data-action="close-lightbox"]');
+const lightboxOverlayRef = document.querySelector('.lightbox__overlay');
+
+modalCloseBtnRef.addEventListener('click', onCloseModal);
+lightboxOverlayRef.addEventListener('click', onLightboxOverlayClick);
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
+  modalWindowRef.classList.remove('is-open');
+  lightboxImageRef.src = '';
+}
+
+function onLightboxOverlayClick(evt) {
+  if (evt.currentTarget === evt.target) {
+    onCloseModal()
+  }
+}
+
+function onEscKeyPress(evt) {
+  if (evt.code === "Escape") {
+    onCloseModal()
+  }
+}
+
+/* Additional task - lazy loading */
+
+if ('loading' in HTMLImageElement.prototype) {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+  lazyImages.forEach(img => {
+    img.src = img.dataset.src;
+  });
+} else {
+  const script = document.createElement('script');
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+  script.integrity = 'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+  script.crossOrigin = 'anonymous';
+
+  document.body.appendChild(script);
 }
